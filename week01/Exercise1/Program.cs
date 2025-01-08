c8c995571b4e7710595e1bfc9815fe2e1eed6400
Diego <diegofrutdfrutosalfonso@byupathway.edu>
2025-01-07 17:45:34 -0700

    Completed tthe first assignment (C# input and output) by writing code that asks for name and last name and displays them James Bond style

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ") ;
+        string fname = Console.ReadLine() ;
+        
+        Console.Write("What is your last name? ") ;
+        string lname = Console.ReadLine() ;
+
+        Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.") ;
     }
 }
