353c30ae39b9b00b4cdcf85ed393db70ec09264f
Diego <diegofrutdfrutosalfonso@byupathway.edu>
2025-01-08 18:09:49 -0700

    Completed the core and the sretch requirements of the 'C# Programming Exercise 2: If Statements' assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        char letterGrade ;
+        int numGrade ;
+        string sign = String.Empty;
+        string article ;
+
+        Console.Write("What's your grade percentage? ");
+        numGrade = int.Parse(Console.ReadLine());
+
+        if (numGrade >= 90) 
+        {
+            letterGrade = 'A' ;
+        }
+        else if (90 > numGrade && numGrade >= 80)
+        {
+            letterGrade = 'B' ;
+        }
+        else if (80 > numGrade && numGrade >= 70)
+        {
+            letterGrade = 'C' ;
+        }
+        else if (70 > numGrade  && numGrade >= 60)
+        {
+            letterGrade = 'D' ;
+        }
+        else 
+        {
+            letterGrade = 'F' ;
+        }
+        if (numGrade < 97 && numGrade >= 60 )
+        {
+            if (numGrade % 10 >= 7 )
+            {
+                sign = "+" ;
+            }
+            else if (numGrade % 10 <= 3)
+            {
+                sign = "-" ;
+            }
+        }
+        
+        if (letterGrade == 'A' || letterGrade == 'B')
+        {
+            article = "an" ;
+        }
+        else
+        {
+            article = "a" ;
+        }
+
+        Console.WriteLine($"\nYou got {article} {letterGrade}{sign}");
+        
+        if (numGrade >= 70) 
+        {
+            Console.WriteLine("Congratulations! You have passed the course!\n");
+        }
+        else 
+        {
+            Console.WriteLine("You have not passed the course at this time, but we encourage you to keep studying and try again next semester!\n");
+        }
     }
 }
