649af08f2524b2edf1416830f0e0cdbb8a2f5d82
Diego <diegofrutdfrutosalfonso@byupathway.edu>
2025-01-08 18:46:46 -0700

    Finished core requirements and stretch challenges of exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random() ;
+        string playAgain = "no";
+
+        do {
+            int guess = 0;
+            int guessCounter = 1;
+            int magicNumber = randomGenerator.Next(1, 100);
+            Console.WriteLine("\nThere is a 'Magic Number' you need to guess.");
+            Console.WriteLine("Let's see how many guesses it takes you to get it.");
+            Console.WriteLine("Pick a number in between 1 and 100.");
+            guess = int.Parse(Console.ReadLine());
+
+            while (guess != magicNumber) 
+            {
+                if (guess > magicNumber) 
+                {
+                    Console.WriteLine("Incorrect, try to guess lower.");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Incorrect, try to guess higher.");
+                }
+                guessCounter = guessCounter + 1 ;
+                guess = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"You're Right! Congratulations! The magic number was {magicNumber}.");
+            Console.WriteLine($"Number of guesses: {guessCounter}");
+
+            Console.WriteLine("Do you want to play again? (Enter 'yes' or 'no')");
+            playAgain = Console.ReadLine() ;
+
+        }while (playAgain == "yes") ;
+        Console.WriteLine("Thank you for playing! See you soon!");
     }
 }
