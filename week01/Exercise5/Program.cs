49240a25038ca999c00645e168cdd0e5919c1ea3
Diego <diegofrutdfrutosalfonso@byupathway.edu>
2025-01-09 21:27:37 -0700

    Finished all of the assignments

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!") ;
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Hi, User!. What's your name? ") ;
+        string name = Console.ReadLine() ;
+        return name ;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What's your favorite number? ") ;
+        int favoriteNumber = int.Parse(Console.ReadLine()) ;
+        return favoriteNumber ;
+    }
+
+    static double SquareNumber(double number )
+    {
+        return Math.Pow(number, 2);
+    }
+
+    static void DisplayResult(string userName, double squareNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareNum}");
+    }
+    
 }
